79f439872ced01b51a412d93151d7f20b838ec56
Ystaw <kristinkasvich@gmail.com>
2025-01-20 15:17:34 +0300

    feat: 02 task

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -3,3 +3,36 @@ const path = require('path');
 const readline = require('readline');
 
 const outputPath = path.join(__dirname, 'output.txt');
+
+const writeStream = fs.createWriteStream(outputPath, { flags: 'a' });
+
+const readl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+console.log('Hello! Enter something! Or type "exit" to quit.');
+
+const handleInput = (input) => {
+    if (input.trim().toLowerCase() === 'exit') {
+        console.log('Bye!');
+        readl.close();
+        process.exit(0);
+    } else {
+        writeStream.write(`${input}\n`, (err) => {
+            if (err) {
+                console.error('Error writing to file:', err);
+            } else {
+                console.log('Success! Please enter more text or type "exit" to quit.');
+            }
+        });
+    }
+};
+
+readl.on('line', handleInput);
+
+process.on('SIGINT', () => {
+    console.log('Bye!');
+    rl.close();
+    process.exit(0);
+});
